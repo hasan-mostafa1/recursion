824d4d46eed943db17ef3479c35710e65b7c03d9
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-10-17 23:26:49 +0300

    Add fibs and fibsRec functions to solve fibonacci
    Those two functions will solve the fibonacci sequence problem
    in two different ways.
    
    fibs will solve the problem using iteration,
    while fibsRec will solve it using recursion.

---
+++ fibonacci.js
@@ -0,0 +1,26 @@
+function fibs(num) {
+  const result = [];
+  for (let i = 0; i < num; i++) {
+    if (i < 2) {
+      result[i] = i;
+    } else {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+  }
+  return result;
+}
+
+// console.log(fibs(8));
+
+function fibsRec(num) {
+  if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return [0, 1];
+  }
+
+  const arr = fibsRec(num - 1);
+  return [...arr, arr.pop() + arr.pop()];
+}
+
+console.log(fibsRec(8));
